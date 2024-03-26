81d1078da614cc76b4007deac22b8a2b81a137e7
Moises T <47845847+mtaira94@users.noreply.github.com>
2024-03-26 07:54:50 -0700

    Finished module 3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit{
+  constructor(){
+    console.log('In constructor');
+    
+  }
+  ngOnInit(): void {
+      console.log('On Init');
+      
+  }
 }
