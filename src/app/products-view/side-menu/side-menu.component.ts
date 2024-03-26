0de9e94bea53f4bf15271b9f09744b9110520abf
Moises T <47845847+mtaira94@users.noreply.github.com>
2024-03-26 13:07:35 -0700

    Finished module 7

--- src/app/products-view/side-menu/side-menu.component.ts
+++ src/app/products-view/side-menu/side-menu.component.ts
@@ -3,6 +3,7 @@ import { PieService } from '../../services/pie.service';
 import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { tap } from 'rxjs';
+import { PRODUCT_ROUTER_TOKENS } from '../products.routes';
 
 @Component({
   standalone: true,
@@ -20,8 +21,10 @@ import { tap } from 'rxjs';
 export class SideMenuComponent {
   @Input() customize = false;
   pies$ = this.pieService.filteredPies$;
+  readonly customizeLink = `./${PRODUCT_ROUTER_TOKENS.CUSTOMIZE}`
+  readonly detailLink = `./${PRODUCT_ROUTER_TOKENS.DETAIL}`
 
   constructor(
     private readonly pieService: PieService,
-  ) {}
+  ) { }
 }
